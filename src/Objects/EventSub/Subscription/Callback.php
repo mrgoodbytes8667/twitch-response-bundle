c3ee93dc121450f237fd890e78a25005b6d3c67e
MrGoodBytes <bytes@goodbytes.live>
2020-11-16 15:18:26 -0600

    Added callback class

---
+++ src/Objects/EventSub/Subscription/Callback.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace Bytes\TwitchResponseBundle\Objects\EventSub\Subscription;
+
+
+/**
+ * Class Callback
+ * @package Bytes\TwitchResponseBundle\Objects\EventSub\Subscription
+ */
+class Callback
+{
+    private string $challenge;
+    private Subscription $subscription;
+
+    /**
+     * @return string
+     */
+    public function getChallenge(): string
+    {
+        return $this->challenge;
+    }
+
+    /**
+     * @param string $challenge
+     * @return $this
+     */
+    public function setChallenge(string $challenge): self
+    {
+        $this->challenge = $challenge;
+        return $this;
+    }
+
+    /**
+     * @return Subscription
+     */
+    public function getSubscription(): Subscription
+    {
+        return $this->subscription;
+    }
+
+    /**
+     * @param Subscription $subscription
+     * @return $this
+     */
+    public function setSubscription(Subscription $subscription): self
+    {
+        $this->subscription = $subscription;
+        return $this;
+    }
+
+
+}
