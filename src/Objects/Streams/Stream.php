b7548cb61c95d373e1faeda2308751e34e9529d9
MrGoodBytes <bytes@goodbytes.live>
2021-06-30 09:49:23 -0500

    Stream should implement TwitchDateTimeInterface

--- src/Objects/Streams/Stream.php
+++ src/Objects/Streams/Stream.php
@@ -4,13 +4,14 @@
 namespace Bytes\TwitchResponseBundle\Objects\Streams;
 
 
+use Bytes\TwitchResponseBundle\Objects\Interfaces\TwitchDateTimeInterface;
 use Bytes\TwitchResponseBundle\Objects\Traits\StreamTrait;
 
 /**
  * Class Stream
  * @package Bytes\TwitchResponseBundle\Objects\Streams
  */
-class Stream
+class Stream implements TwitchDateTimeInterface
 {
     use StreamTrait;
 }
