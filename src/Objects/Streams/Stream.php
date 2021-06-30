2baca8b5a6737554350d7d5984a0e9794378b51a
MrGoodBytes <bytes@goodbytes.live>
2021-06-30 09:41:12 -0500

    Add Stream/StreamResponse models

---
+++ src/Objects/Streams/Stream.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Bytes\TwitchResponseBundle\Objects\Streams;
+
+
+use Bytes\TwitchResponseBundle\Objects\Traits\StreamTrait;
+
+/**
+ * Class Stream
+ * @package Bytes\TwitchResponseBundle\Objects\Streams
+ */
+class Stream
+{
+    use StreamTrait;
+}
