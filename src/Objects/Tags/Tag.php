1a164d53d531d7354b014f62c930db822f3cef31
mrgoodbytes8667 <72107446+mrgoodbytes8667@users.noreply.github.com>
2021-07-21 16:30:53 -0500

    Add stream tags models (#26)

---
+++ src/Objects/Tags/Tag.php
@@ -0,0 +1,117 @@
+<?php
+
+
+namespace Bytes\TwitchResponseBundle\Objects\Tags;
+
+
+use Symfony\Component\Serializer\Annotation\SerializedName;
+
+/**
+ * Class Tag
+ * @package Bytes\TwitchResponseBundle\Objects\Tags
+ *
+ * @link https://dev.twitch.tv/docs/api/reference#get-all-stream-tags
+ */
+class Tag
+{
+    /**
+     * @var string|null
+     */
+    private $tag_id;
+
+    /**
+     * @var bool|null
+     * @SerializedName("is_auto")
+     */
+    private $auto;
+
+    /**
+     * @var string[]|null
+     */
+    private $localization_names;
+
+    /**
+     * @var string[]|null
+     */
+    private $localization_descriptions;
+
+    /**
+     * ID of the tag.
+     * @return string|null
+     */
+    public function getTagId(): ?string
+    {
+        return $this->tag_id;
+    }
+
+    /**
+     * ID of the tag.
+     * @param string|null $tag_id
+     * @return $this
+     */
+    public function setTagId(?string $tag_id): self
+    {
+        $this->tag_id = $tag_id;
+        return $this;
+    }
+
+    /**
+     * true if the tag is auto-generated; otherwise, false . An auto-generated tag is one automatically applied by Twitch (e.g., a language tag based on the broadcaster’s settings); these tags cannot be added or removed by the user.
+     * @return bool|null
+     */
+    public function isAuto(): ?bool
+    {
+        return $this->auto;
+    }
+
+    /**
+     * true if the tag is auto-generated; otherwise, false . An auto-generated tag is one automatically applied by Twitch (e.g., a language tag based on the broadcaster’s settings); these tags cannot be added or removed by the user.
+     * @param bool|null $auto
+     * @return $this
+     */
+    public function setAuto(?bool $auto): self
+    {
+        $this->auto = $auto;
+        return $this;
+    }
+
+    /**
+     * All localized names of the tag.
+     * @return string[]|null
+     */
+    public function getLocalizationNames(): ?array
+    {
+        return $this->localization_names;
+    }
+
+    /**
+     * All localized names of the tag.
+     * @param string[]|null $localization_names
+     * @return $this
+     */
+    public function setLocalizationNames(?array $localization_names): self
+    {
+        $this->localization_names = $localization_names;
+        return $this;
+    }
+
+    /**
+     * All localized descriptions of the tag.
+     * @return string[]|null
+     */
+    public function getLocalizationDescriptions(): ?array
+    {
+        return $this->localization_descriptions;
+    }
+
+    /**
+     * All localized descriptions of the tag.
+     * @param string[]|null $localization_descriptions
+     * @return $this
+     */
+    public function setLocalizationDescriptions(?array $localization_descriptions): self
+    {
+        $this->localization_descriptions = $localization_descriptions;
+        return $this;
+    }
+}
---
+++ src/Objects/Tags/TagsResponse.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace Bytes\TwitchResponseBundle\Objects\Tags;
+
+
+use Bytes\TwitchResponseBundle\Objects\Interfaces\DataPaginationResponseInterface;
+use Bytes\TwitchResponseBundle\Objects\Traits\PaginationTrait;
+
+/**
+ * Class TagsResponse
+ * @package Bytes\TwitchResponseBundle\Objects\Streams
+ *
+ * @link https://dev.twitch.tv/docs/api/reference#get-all-stream-tags
+ */
+class TagsResponse implements DataPaginationResponseInterface
+{
+    use PaginationTrait;
+
+    /**
+     * @var Tag[]|null
+     */
+    public $data;
+
+    /**
+     * @return Tag[]|null
+     */
+    public function getData(): ?array
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param Tag[]|null $data
+     * @return $this
+     */
+    public function setData(?array $data): self
+    {
+        $this->data = $data;
+        return $this;
+    }
+}
