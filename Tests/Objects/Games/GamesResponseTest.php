71ad7e4893a507892269dbc10928fa166d90a0f6
MrGoodBytes <bytes@goodbytes.live>
2021-08-12 16:09:13 -0500

    Add some functions to partially implement Doctrine/Common/Collection

---
+++ Tests/Objects/Games/GamesResponseTest.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Bytes\TwitchResponseBundle\Tests\Objects\Games;
+
+use Bytes\Common\Faker\Twitch\TestTwitchFakerTrait;
+use Bytes\TwitchResponseBundle\Objects\Games\Game;
+use Bytes\TwitchResponseBundle\Objects\Games\GamesResponse;
+use Generator;
+use PHPUnit\Framework\TestCase;
+
+/**
+ *
+ */
+class GamesResponseTest extends TestCase
+{
+    use TestTwitchFakerTrait;
+
+    /**
+     * @dataProvider provideGames
+     * @param $games
+     * @param $count
+     * @param $first
+     * @param $last
+     */
+    public function testGetSetData($games, $count, $first, $last)
+    {
+        $g = new GamesResponse();
+        $this->assertNull($g->getData());
+        $this->assertInstanceOf(GamesResponse::class, $g->setData(null));
+        $this->assertNull($g->getData());
+        $this->assertInstanceOf(GamesResponse::class, $g->setData([]));
+        $this->assertCount(0, $g->getData());
+
+        $this->assertInstanceOf(GamesResponse::class, $g->setData($games));
+        $this->assertCount($count, $g->getData());
+        $this->assertEquals($games, $g->getData());
+
+        return $g;
+    }
+
+    /**
+     * @return Generator
+     */
+    public function provideGames()
+    {
+        $this->setupFaker();
+
+        $games = [];
+        $firstGame = $this->buildGame();
+        $games[] = $firstGame;
+        foreach (range(1, 3) as $i) {
+            $games[] = $this->buildGame();
+        }
+        $lastGame = $this->buildGame();
+        $games[] = $lastGame;
+
+        yield ['games' => $games, 'count' => 5, 'first' => $firstGame, 'last' => $lastGame];
+    }
+
+    /**
+     * @return Game
+     */
+    private function buildGame(): Game
+    {
+        $game = new Game();
+        $game->setGameID($this->faker->id())
+            ->setName($this->faker->sentence(2))
+            ->setBoxArtURL($this->faker->imageUrl());
+        return $game;
+    }
+
+    /**
+     * @dataProvider provideGames
+     * @param $games
+     * @param $count
+     * @param $first
+     * @param $last
+     */
+    public function testCount($games, $count, $first, $last)
+    {
+        $g = new GamesResponse();
+        $g->setData($games);
+
+        $this->assertCount($count, $g->getData());
+        $this->assertEquals($count, $g->count());
+    }
+
+    /**
+     * @dataProvider provideGames
+     * @param $games
+     * @param $count
+     * @param $first
+     * @param $last
+     */
+    public function testFirst($games, $count, $first, $last)
+    {
+        $g = new GamesResponse();
+        $g->setData($games);
+
+        $this->assertEquals($first, $g->first());
+    }
+
+    /**
+     * @dataProvider provideGames
+     * @param $games
+     * @param $count
+     * @param $first
+     * @param $last
+     */
+    public function testLast($games, $count, $first, $last)
+    {
+        $g = new GamesResponse();
+        $g->setData($games);
+
+        $this->assertEquals($last, $g->last());
+    }
+
+    /**
+     * @dataProvider provideGames
+     * @param $games
+     * @param $count
+     * @param $first
+     * @param $last
+     */
+    public function testGet($games, $count, $first, $last)
+    {
+        $g = new GamesResponse();
+        $g->setData($games);
+
+        $game = $g->getData()[2];
+        $this->assertEquals($game, $g->get(2));
+    }
+
+    /**
+     * @dataProvider provideGames
+     * @param $games
+     * @param $count
+     * @param $first
+     * @param $last
+     */
+    public function testPointers($games, $count, $first, $last)
+    {
+        $g = new GamesResponse();
+        $g->setData($games);
+
+        $game = $g->getData()[2];
+        $this->assertEquals($game, $g->get(2));
+
+        $this->assertEquals($first, $g->first());
+        $this->assertEquals($game, $g->get(2));
+        $this->assertEquals($last, $g->last());
+        $this->assertEquals($game, $g->get(2));
+        $this->assertEquals($last, $g->last());
+        $this->assertEquals($game, $g->get(2));
+        $this->assertEquals($first, $g->first());
+    }
+}
--- src/Objects/Games/GamesResponse.php
+++ src/Objects/Games/GamesResponse.php
@@ -5,12 +5,13 @@ namespace Bytes\TwitchResponseBundle\Objects\Games;
 
 
 use Bytes\TwitchResponseBundle\Objects\Traits\PaginationTrait;
+use Countable;
 
 /**
  * Class GamesResponse
  * @package Bytes\TwitchResponseBundle\Objects\Games
  */
-class GamesResponse
+class GamesResponse implements Countable
 {
     use PaginationTrait;
 
@@ -35,5 +36,47 @@ class GamesResponse
     {
         $this->data = $data;
         return $this;
+    }
+
+    /**
+     * Count elements of an object
+     * @link https://php.net/manual/en/countable.count.php
+     * @return int The custom count as an integer.
+     */
+    public function count()
+    {
+        return count($this->data);
+    }
+
+    /**
+     * Sets the internal iterator to the first element in the collection and returns this element.
+     *
+     * @return Game|null
+     */
+    public function first()
+    {
+        return reset($this->data);
+    }
+
+    /**
+     * Sets the internal iterator to the last element in the collection and returns this element.
+     *
+     * @return Game|null
+     */
+    public function last()
+    {
+        return end($this->data);
+    }
+
+    /**
+     * Gets the element at the specified key/index.
+     *
+     * @param string|int $key The key/index of the element to retrieve.
+     *
+     * @return Game|null
+     */
+    public function get($key)
+    {
+        return $this->data[$key] ?? null;
     }
 }
