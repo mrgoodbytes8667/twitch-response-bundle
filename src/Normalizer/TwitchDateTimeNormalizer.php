38c5a4132ffc44e1872a86664631a56407d4518f
mrgoodbytes8667 <72107446+mrgoodbytes8667@users.noreply.github.com>
2021-11-08 11:12:05 -0600

    Replace calls to Str with Symfony string (#34)

--- src/Normalizer/TwitchDateTimeNormalizer.php
+++ src/Normalizer/TwitchDateTimeNormalizer.php
@@ -5,7 +5,6 @@ namespace Bytes\TwitchResponseBundle\Normalizer;
 
 
 use Bytes\TwitchResponseBundle\Objects\Interfaces\TwitchDateTimeInterface;
-use Illuminate\Support\Str;
 use Symfony\Component\Serializer\Exception\ExceptionInterface;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use function Symfony\Component\String\u;
@@ -39,21 +38,21 @@ class TwitchDateTimeNormalizer extends ObjectNormalizer
                 case 'followed_at':
                 case 'started_at':
                     $camelKey = u($key)->camel()->toString();
-                    $createdAt = Str::of($data[$key]);
+                    $createdAt = u($data[$key]);
                     //0000-00-00T00:00:00.vP
-                    $timezone = $createdAt->when($createdAt->substr(-6, 1) == '+' || $createdAt->substr(-6, 1) == '-' || $createdAt->endsWith('Z'), function ($string) {
-                        if ($string->endsWith('Z')) {
-                            return 'Z';
-                        } elseif ($string->substr(-6, 1) == '+') {
-                            return $string->afterLast('+')->prepend('+');
+                    if(in_array($createdAt->slice(-6, 1)->toString(), ['+', '-']) || $createdAt->endsWith('Z'))
+                    {
+                        if ($createdAt->endsWith('Z')) {
+                            $timezone =  'Z';
+                        } elseif ($createdAt->slice(-6, 1) == '+') {
+                            $timezone =  $createdAt->afterLast('+')->prepend('+');
                         } else {
-                            return $string->afterLast('-')->prepend('-');
+                            $timezone =  $createdAt->afterLast('-')->prepend('-');
                         }
-                    });
-                    $createdAt = $createdAt
-                        ->when($createdAt->contains('.'), function ($string) use ($timezone) {
-                            return $string->before('.')->append($timezone);
-                        });
+                        if($createdAt->containsAny('.')) {
+                            $createdAt = $createdAt->before('.')->append($timezone);
+                        }
+                    }
 
                     $data[$camelKey] = (string)$createdAt;
                     unset($data[$key]);
