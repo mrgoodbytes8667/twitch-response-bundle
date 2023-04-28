84cb5e45b65f5ab501e3480a4104fbf2c6bc77fc
mrgoodbytes8667 <72107446+mrgoodbytes8667@users.noreply.github.com>
2023-04-28 11:26:21 -0500

    Add GameInterface, getBoxArtURLWithSize() (#42)

--- src/Objects/Games/Game.php
+++ src/Objects/Games/Game.php
@@ -4,15 +4,19 @@
 namespace Bytes\TwitchResponseBundle\Objects\Games;
 
 
+use Bytes\TwitchResponseBundle\Objects\ImageResize;
+use Bytes\TwitchResponseBundle\Objects\Interfaces\GameInterface;
 use Symfony\Component\Serializer\Annotation\SerializedName;
 
 /**
  * Class Game
  * @package Bytes\TwitchResponseBundle\Objects\Games
+ *
+ * @link https://dev.twitch.tv/docs/api/reference/#get-games
+ * @version As of 2023-04-28
  */
-class Game
+class Game implements GameInterface
 {
-
     /**
      * @var string|int|null
      */
@@ -27,7 +31,14 @@ class Game
     /**
      * @var string|null
      */
+    #[SerializedName('box_art_url')]
     private ?string $boxArtURL = '';
+
+    /**
+     * @var string|null
+     */
+    #[SerializedName('igdb_id')]
+    private ?string $igdbId = '';
 
     /**
      * @return string|null
@@ -74,6 +85,16 @@ class Game
     }
 
     /**
+     * @param int $width
+     * @param int $height
+     * @return string
+     */
+    public function getBoxArtURLWithSize(int $width = ImageResize::WIDTH_TWITCH_GAME_THUMBNAIL, int $height = ImageResize::HEIGHT_TWITCH_GAME_THUMBNAIL): string
+    {
+        return ImageResize::twitchGameThumbnail(url: $this->boxArtURL ?? '', width: $width, height: $height);
+    }
+
+    /**
      * @param string|null $boxArtURL
      * @return $this
      */
@@ -84,17 +105,37 @@ class Game
     }
 
     /**
+     * @return string|null
+     */
+    public function getIgdbId(): ?string
+    {
+        return $this->igdbId;
+    }
+
+    /**
+     * @param string|null $igdbId
+     * @return $this
+     */
+    public function setIgdbId(?string $igdbId): self
+    {
+        $this->igdbId = $igdbId;
+        return $this;
+    }
+
+    /**
      * @param int|string $gameId
      * @param string $name
      * @param string $boxArtUrl
+     * @param string $igdbId
      * @return static
      */
-    public static function make(int|string $gameId, string $name, string $boxArtUrl): static
+    public static function make(int|string $gameId, string $name, string $boxArtUrl, string $igdbId = ''): static
     {
         $static = new static();
         $static->setGameID($gameId)
             ->setName($name)
-            ->setBoxArtURL($boxArtUrl);
+            ->setBoxArtURL($boxArtUrl)
+        ->setIgdbId($igdbId);
         return $static;
     }
 }
