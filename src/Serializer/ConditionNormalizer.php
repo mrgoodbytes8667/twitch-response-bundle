8fb83b27a42ebdeb4c6bb4815c83c66dd972c4f3
MrGoodBytes <bytes@mrgoodbytes.dev>
2023-05-10 10:07:07 -0500

    Add getSupportedTypes() method to normalizers

--- src/Serializer/ConditionNormalizer.php
+++ src/Serializer/ConditionNormalizer.php
@@ -4,6 +4,7 @@
 namespace Bytes\TwitchResponseBundle\Serializer;
 
 
+use ArrayObject;
 use Bytes\TwitchResponseBundle\Objects\EventSub\Subscription\Condition;
 use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 
@@ -16,7 +17,7 @@ class ConditionNormalizer extends GetSetMethodNormalizer
     /**
      * {@inheritdoc}
      */
-    public function normalize($object, string $format = null, array $context = []): array|string|int|float|bool|\ArrayObject|null
+    public function normalize($object, string $format = null, array $context = []): array|string|int|float|bool|ArrayObject|null
     {
         return parent::normalize($object, $format, $context);
     }
@@ -46,5 +47,27 @@ class ConditionNormalizer extends GetSetMethodNormalizer
     public function supportsNormalization($data, string $format = null): bool
     {
         return ($data instanceof Condition) && parent::supportsNormalization($data, $format);
+    }
+
+    /**
+     * Returns the types potentially supported by this [de]normalizer.
+     *
+     * For each supported formats (if applicable), the supported types should be
+     * returned as keys, and each type should be mapped to a boolean indicating
+     * if the result of supports[Den|N]ormalization() can be cached or not
+     * (a result cannot be cached when it depends on the context or on the data.)
+     * A null value means that the [de]normalizer does not support the corresponding
+     * type.
+     *
+     * Use type "object" to match any classes or interfaces,
+     * and type "*" to match any types.
+     *
+     * @return array<class-string|'*'|'object'|string, bool|null>
+     */
+    public function getSupportedTypes(?string $format): array
+    {
+        return [
+            Condition::class => (__CLASS__ === static::class),
+        ];
     }
 }
